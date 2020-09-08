18888a30a2a9c19da25a3dc189a054c769f6350d
unknown <sreevasrao@gmail.com>
2020-09-08 11:25:15 +0530

    first commit

---
+++ zuulserver/src/main/java/com/example/zuuldemo/filter/AddRequestHeaderFilter.java
@@ -0,0 +1,33 @@
+package com.example.zuuldemo.filter;
+
+import com.netflix.zuul.ZuulFilter;
+import com.netflix.zuul.context.RequestContext;
+import com.netflix.zuul.exception.ZuulException;
+
+public class AddRequestHeaderFilter extends ZuulFilter {
+
+	@Override
+	public boolean shouldFilter() {
+		// TODO Auto-generated method stub
+		return true;
+	}
+
+	@Override
+	public Object run() throws ZuulException {
+		RequestContext ctx = RequestContext.getCurrentContext();
+		ctx.addZuulRequestHeader("x-location", "India");
+		return null;
+	}
+
+	@Override
+	public String filterType() {
+		return "pre";
+	}
+
+	@Override
+	public int filterOrder() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+}
